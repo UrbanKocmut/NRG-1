2d98b1bb1225f484ac4717bf2466392311a07421
UrbanKocmut <u.kocmut@gmail.com>
2018-11-03 23:57:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# CYKA http://tizian.cs.uni-bonn.de/publications/BaudsonKlein.pdf
+
+class Tetraheder:
+
+    def centerPoint(self):
+
+        pass
+
+    def __init__(self, points):
+        self.points = np.array(points)
